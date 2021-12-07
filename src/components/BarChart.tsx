a97730abf86295fe3534c2060eb22d4a7948e568
Ellis Whitehead <ellis@ellisw.net>
2021-12-07 16:20:32 +0100

    chore: notes from meeting

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,33 +1,38 @@
-import createBarChart from 'lib/createBarChart';
-import drawImage from 'lib/drawImage';
-import { memo, useEffect, useRef } from 'react';
+import createBarChart from "lib/createBarChart";
+import drawImage from "lib/drawImage";
+import React, { useRef, useEffect } from "react";
 
-export type BarChartProps = {
-  barWidth?: number;
-  className?: string;
-  color?: string;
-  gap?: number;
-  size: number;
-  values: number[];
-};
+export interface BarChartProps {
+	readonly barWidth?: number;
+	readonly className?: string;
+	readonly color?: string;
+	readonly gap?: number;
+	readonly size: number;
+	readonly values: number[];
+}
 
-export const BarChart = memo(
-  ({ barWidth = 20, className, color, gap = 0, size, values }: BarChartProps) => {
-    const height = size;
-    const width = (values.length - 1) * (barWidth + gap) + barWidth;
-    const ref = useRef<HTMLCanvasElement>(null);
+export function BarChart({
+	barWidth = 20,
+	className,
+	color,
+	gap = 0,
+	size,
+	values,
+}: BarChartProps): JSX.Element {
+	const height = size;
+	const width = (values.length - 1) * (barWidth + gap) + barWidth;
+	const ref = useRef<HTMLCanvasElement>(null);
 
-    useEffect(() => {
-      const component = createBarChart({ barWidth, color, gap, height, values, width });
-      const container = ref.current;
-      const context = container?.getContext('2d');
+	useEffect(() => {
+		const component = createBarChart({ barWidth, color, gap, height, values, width });
+		const container = ref.current;
+		const context = container?.getContext("2d");
 
-      if (component && container && context) {
-        context.clearRect(0, 0, container.width, container.height);
-        drawImage({ component, container });
-      }
-    }, [barWidth, color, gap, height, size, values, width]);
+		if (component && container && context) {
+			context.clearRect(0, 0, container.width, container.height);
+			drawImage({ component, container });
+		}
+	}, [barWidth, color, gap, height, size, values, width]);
 
-    return <canvas className={className} height={height} ref={ref} width={width} />;
-  }
-);
+	return <canvas className={className} height={height} ref={ref} width={width} />;
+}
