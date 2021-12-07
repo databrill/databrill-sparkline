f1cf0a4d770800a7c05b694126214a6a3a6e39ad
wanhose <wanhose.development@gmail.com>
2021-12-07 12:12:22 +0100

    feat(lib): create bar chart and canvas utilities

---
+++ src/lib/__tests__/drawImage.test.ts
@@ -0,0 +1,27 @@
+import createBarChart from '../createBarChart';
+import drawImage from '../drawImage';
+
+describe('drawImage', () => {
+  it('must draw bar chart', () => {
+    const blank = document.createElement('canvas');
+    const height = 50;
+    const width = 92;
+    const canvas = document.createElement('canvas');
+    canvas.setAttribute('height', `${height}`);
+    canvas.setAttribute('width', `${width}`);
+
+    const image = createBarChart({
+      barWidth: 20,
+      gap: 0,
+      height,
+      values: [0, 1, 2, 3],
+      width,
+    });
+
+    // eslint-disable-next-line
+    // @ts-ignore
+    drawImage({ component: image, container: canvas });
+
+    expect(canvas.toDataURL()).not.toBe(blank.toDataURL());
+  });
+});
---
+++ src/lib/drawImage.ts
@@ -0,0 +1,18 @@
+export type Props = {
+  component: SVGSVGElement;
+  container: HTMLCanvasElement;
+};
+
+const drawImage = ({ component, container }: Props) => {
+  const xdocument = document.implementation.createDocument('http://www.w3.org/1999/xhtml', 'html');
+  xdocument.documentElement.appendChild(component);
+
+  const context = container.getContext('2d');
+  const dataUrl = 'data:image/svg+xml,' + window.encodeURIComponent(component.outerHTML);
+  const image = new Image();
+
+  image.addEventListener('load', () => context?.drawImage(image, 0, 0));
+  image.setAttribute('src', dataUrl);
+};
+
+export default drawImage;
