fa0e3e673a032da94a40cd24f4510a1ea21cc4d9
wanhose <wanhose.development@gmail.com>
2021-12-07 12:10:51 +0100

    chore: update configs

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,1 @@
-export * from 'components/BarChart';
+export * from './BarChart';
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-dts';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 import path from 'path';
@@ -13,7 +14,12 @@ export default defineConfig({
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
     },
   },
-  plugins: [react(), tsconfigPaths()],
+  plugins: [dts(), react(), tsconfigPaths()],
 });
