43b31b44e38fecf73794e67c9624375727ddd0ca
wanhose <wanhose.development@gmail.com>
2021-12-27 15:56:00 +0100

    fix(components): scatter plot test

--- src/components/__tests__/ScatterPlot.test.tsx
+++ src/components/__tests__/ScatterPlot.test.tsx
@@ -4,7 +4,9 @@ import { ScatterPlot } from "../ScatterPlot";
 describe("ScatterPlot", () => {
 	it("must accept custom classes", () => {
 		const className = "jest";
-		const { container } = render(<ScatterPlot className={className} size={64} x={[]} y={[]} />);
+		const { container } = render(
+			<ScatterPlot className={className} size={64} x={[1]} y={[1]} />
+		);
 		const component = container.getElementsByClassName(className)[0];
 
 		expect(component).toBeInTheDocument();
