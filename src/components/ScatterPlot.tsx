c8bfcf349e241093ccb106c1eae3f6cf1eb9c4a4
wanhose <wanhose.development@gmail.com>
2021-12-28 09:52:13 +0100

    feat(components): fourth version of scatter plot

--- src/components/ScatterPlot.tsx
+++ src/components/ScatterPlot.tsx
@@ -1,10 +1,15 @@
+import useDebounceCallback from "hooks/useDebounceCallback";
 import { drawCircle, drawLine, drawRectangleOutline, drawText, setup } from "lib/canvas";
 import { isValid } from "lib/color";
-import { memo, useEffect, useMemo, useRef } from "react";
+import { memo, MouseEvent, useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { Tooltip, TooltipProps } from "./Tooltip";
+
+/* eslint-disable jsx-a11y/mouse-events-have-key-events */
 
 export interface ScatterPlotProps {
 	readonly className?: string;
 	readonly dotColor?: string;
+	readonly dotHighlightColor?: string;
 	readonly dotSize?: number;
 	readonly size: number;
 	readonly stepX?: number;
@@ -17,6 +22,7 @@ export const ScatterPlot = memo(
 	({
 		className,
 		dotColor = "black",
+		dotHighlightColor = "red",
 		dotSize = 8,
 		size,
 		stepX,
@@ -34,6 +40,7 @@ export const ScatterPlot = memo(
 		const plotHeight = size - gap - 1;
 		const plotWidth = size - gap - 1;
 		const ref = useRef<HTMLCanvasElement>(null);
+		const [tooltip, setTooltip] = useState<TooltipProps | null>(null);
 
 		const items = useMemo(() => {
 			const itemColor = isValid(dotColor) ? dotColor : "black";
@@ -46,8 +53,35 @@ export const ScatterPlot = memo(
 				size: itemSize,
 				x: Math.round(((value + -minX) * position) / rangeX) + gap + itemSize,
 				y: Math.round((((y[index] ?? 0) + -minY) * position) / rangeY) + gap + itemSize,
+				value: `${value}, ${y[index] ?? 0}`,
 			}));
 		}, [dotColor, itemSize, maxX, maxY, minX, minY, size, x, y]);
+
+		const handleMouseMove = useDebounceCallback(
+			(event: MouseEvent<HTMLCanvasElement>): void => {
+				const canvas = ref.current;
+				const bound = canvas?.getBoundingClientRect();
+				const x = event.clientX - (bound?.left ?? 0) - (canvas?.clientLeft ?? 0);
+				const y = event.clientY - (bound?.top ?? 0) - (canvas?.clientTop ?? 0);
+				const current = items.find((item) => x >= item.x && x <= item.x + item.size);
+
+				if (current)
+					setTooltip({ hidden: false, left: x + 8, top: y - 16, value: current?.value });
+				else setTooltip(null);
+
+				items.forEach((item) => {
+					const active = current?.x === item.x;
+					drawCircle({ ...item, canvas, color: active ? dotHighlightColor : item.color });
+				});
+			},
+			[dotHighlightColor, items]
+		);
+
+		const handleMouseOut = useCallback(() => {
+			handleMouseMove.cancel();
+			items.forEach((item) => drawCircle({ ...item, canvas: ref.current }));
+			setTooltip(null);
+		}, [handleMouseMove, items]);
 
 		useEffect(() => {
 			const canvas = ref.current;
@@ -111,7 +145,13 @@ export const ScatterPlot = memo(
 
 		return (
 			<div className={className} style={{ display: "inline-flex", position: "relative" }}>
-				<canvas ref={ref} />
+				<canvas onMouseMove={handleMouseMove} onMouseOut={handleMouseOut} ref={ref} />
+				<Tooltip
+					hidden={tooltip?.hidden}
+					left={tooltip?.left}
+					top={tooltip?.top}
+					value={tooltip?.value}
+				/>
 			</div>
 		);
 	}
