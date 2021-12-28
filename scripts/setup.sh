59be33b784685ca127552e906928c1bdf901b78e
wanhose <wanhose.development@gmail.com>
2021-12-28 11:39:18 +0100

    feat(scripts): add scatter plot to index tsx echo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,4 +7,4 @@ husky add .husky/commit-msg 'npx --no -- commitlint --edit "$1"'
 husky add .husky/pre-push "yarn test"
 
 echo '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /></head><body><div id="root"></div><script type="module" src="/src/index.tsx"></script></body></html>' > index.html
-echo 'import { BarChart } from "./components";import ReactDOM from "react-dom";ReactDOM.render(<BarChart gap={4} highlightColor="red" size={50} values={[-1, -2, -3, -4, -20]} />,document.querySelector("#root"));' > src/index.tsx
+echo 'import { BarChart,ScatterPlot } from "./components";import ReactDOM from "react-dom";ReactDOM.render(<div style={{ display: "flex", flexDirection: "column" }}><BarChart size={250} values={[-10, -5, 0, 10, 15, 20]} /><ScatterPlotdotSize={8}size={250}x={[-1, 12, 7, 5, 1, 10, 9, 15]}y={[0, 3, 12, 4, 14, 5, 10, 15]}/></div>,document.querySelector("#root"));' > src/index.tsx
